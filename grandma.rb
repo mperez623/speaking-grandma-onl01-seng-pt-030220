c84658d3815629c1a7c2187bc3f81e88c11e5553
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-12 02:58:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,1 +1,11 @@
-def 
+def speak_to_grandma
+  if "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif "HELLO GRANDMA!"
+    puts "NO, NOT SINCE 1983!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
+  
