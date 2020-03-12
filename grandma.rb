37a0015ac84f794d01f240e670194b8d71de8313
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-12 03:02:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,9 @@
-def speak_to_grandma
-  if "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif "HELLO GRANDMA!"
+  elsif phrase == "HELLO GRANDMA!"
     puts "NO, NOT SINCE 1983!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
 end
-
-  
